c1baeac97fa0dc0161d0158c5b56b343d38d075a
justiceqaq <justiceqaq@gmail.com>
2022-11-16 13:55:29 +0800

    refactor: observer_pattern

--- code/design_patterns/observer_pattern/heater_helper.py
+++ code/design_patterns/observer_pattern/heater_helper.py
@@ -1,8 +1,25 @@
-class WatterHeater:
-    """熱水器"""
+class Observable:
+    """"""
 
     def __init__(self):
         self._observer = []
+
+    def add_observer(self, observer):
+        self._observer.append(observer)
+
+    def remove_observer(self, observer):
+        self._observer.remove(observer)
+
+    def notify_observer(self, obj=0):
+        for o in self._observer:
+            o.update(self, obj)
+
+
+class WatterHeater(Observable):
+    """熱水器"""
+
+    def __init__(self):
+        super().__init__()
         self._temperature = 25
 
     def get_temperature(self):
@@ -11,11 +28,4 @@ class WatterHeater:
     def set_temperature(self, temperature):
         self._temperature = temperature
         print(f"溫度已設定為 {self._temperature}")
-        self.notifies()
-
-    def add_observer(self, observer):
-        self._observer.append(observer)
-
-    def notifies(self):
-        for o in self._observer:
-            o.update(self)
+        self.notify_observer()
