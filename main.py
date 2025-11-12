1a01b3e00ac1c26680f9df0901fa98a9720e2cad
JustIceQAQ <justiceqaq@gmail.com>
2025-11-13 02:19:40 +0800

    tweak: Parallel Crawlers

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from pathlib import Path
 
 
 async def main(start: int, end: int, output_path: Path):
-    results = {
-        "start": start,
-        "end": end,
-    }
-    with open(output_path / f"{start}_{end}.json", "w", encoding="utf-8") as f:
-        json.dump(results, f, ensure_ascii=False, indent=2)
+    for n in range(start, end):
+        results = {
+            "index": n,
+        }
+        with open(output_path / f"{n}.json", "w", encoding="utf-8") as f:
+            json.dump(results, f, ensure_ascii=False, indent=2)
 
 
 if __name__ == "__main__":
